564b8919dadefe308b6e528e6a9cc31e36efc0a6
martin.rusnak <martin.rusnak@prosoft.sk>
2022-04-15 13:44:21 +0200

    Gradle upgrade. Migration to AndroidX.

--- app/src/main/java/ir/smartdevelop/eram/showcaseview/MainActivity.java
+++ app/src/main/java/ir/smartdevelop/eram/showcaseview/MainActivity.java
@@ -1,7 +1,7 @@
 package ir.smartdevelop.eram.showcaseview;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 
 import smartdevelop.ir.eram.showcaseviewlib.GuideView;
